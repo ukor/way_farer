ece682815f63250ecbadcd58d932436e30e82566
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-06-29 02:16:35 +0100

    Fix build error
    - Added mocha configuration file

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('Initializee Test', function () {
+	it('Expect build to run without failing', function () {
+		expect(1).to.equal(1);
+	})
+});
