7a4bd9618b512eddedc08534cac269ac8d5d891c
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-06-29 04:23:32 +0100

    Created custom error handler

---
+++ server/errorHandles/wayFarerError.js
@@ -0,0 +1,32 @@
+'use strict';
+const mongoErrorContextSymbol = Symbol('mongoErrorContextSymbol');
+/**
+ * Creates a new WayFarer Error
+ *
+ * @augments Error
+ * @param {Error|string|object} message The error message
+ * @property {string} message The error message
+ * @property {string} stack The error call stack
+ */
+class WayFarerError extends Error {
+	constructor(message) {
+		if (message instanceof Error) {
+			super(message.message);
+			this.stack = message.stack;
+		} else {
+			if (typeof message === 'string') {
+				super(message);
+			} else {
+				super(message.message || message.errmsg || message.$err || 'n/a');
+				for (var name in message) {
+					this[name] = message[name];
+				}
+			}
+			Error.captureStackTrace(this, this.constructor);
+		}
+		this.name = 'MongoError';
+		this[mongoErrorContextSymbol] = this[mongoErrorContextSymbol] || {};
+	}
+}
+
+module.exports = WayFarerError;
