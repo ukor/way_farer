43df7919d882c2c28368b1983e5ee15ce2a5d2e6
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-06-29 02:43:51 +0100

    Added test case for root route

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
 const chai = require('chai');
-
 const expect = chai.expect;
+const app = `localhost:${process.env.PORT}`
+const request = require('supertest')(app);
 
 describe('Initializee Test', function () {
 	it('Expect build to run without failing', function () {
 		expect(1).to.equal(1);
-	})
+	});
+
+	describe('Express Setup', function () {
+		it('Expect root route to return status code 200', function (done) {
+			request.get('/').send().expect(200, done);
+		});
+	});
 });
