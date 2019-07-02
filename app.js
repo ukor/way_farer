a86551ab36afbf38d5b65d12acb72f9b717e5c81
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-02 17:32:40 +0100

    Set-up database connection string

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 
+const { Pool } = require('pg');
+const { env } = process;
+
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
+(async function(){
+	const dev_test = `postgres://${env.PGuser}${env.PGpassword ? ':'+env.PGpassword : ''}@${env.PGhost}:${env.PGport}/${env.PGdatabaseName}`;
+
+	const dbURI = env.NODE_ENV === 'production' ? env.DATABASE_URL : dev_test;
+	const dbPool = new Pool({
+		connectionString: dbURI,
+	});
+	// make a single connection available
+	// let dbClient = dbPool.connect();
+	/**
+	 * @see https://node-postgres.com/features/pooling
+	 * The preffered way to query
+	 * @see https://node-postgres.com/features/pooling#single-query
+	 */
+	app.locals.dbClient = dbPool;
+})();
+
+if (env.NODE_ENV === 'production') {
 	app.use(helmet());
 }
 
