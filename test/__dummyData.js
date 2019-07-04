3c76b1f4fda0f609cd9917652262cd4411c865c4
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-04 11:03:41 +0100

    Made user dummy data dynamic

--- test/__dummyData.js
+++ test/__dummyData.js
@@ -1,8 +1,9 @@
+const slug = require('shortid');
 module.exports = {
 	signup: {
-		first_name: 'First',
-		last_name: 'Last',
-		email: 'test@example.dummy',
-		password: 'dummy_that'
+		first_name: `${slug.generate()}`,
+		last_name: `${slug.generate()}`,
+		email: `${slug.generate()}@gmail.test`,
+		password: `${slug.generate()}`
 	}
 };
