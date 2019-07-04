67778d662bf375214e4ac909e23e1f3908e363f6
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-04 19:39:53 +0100

    change db connection string

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const databaseTables = require('./server/models/installDatabaseTable.js');
 const routes = require('./server/routes/index.js');
+const pgConnection = require('pg-connection-string');
 
 const { Pool } = require('pg');
 const { env } = process;
@@ -13,10 +14,11 @@ const app = express();
 	const dev_test = `postgres://${env.PGuser}:${env.PGpassword}@${env.PGhost}:${env.PGport}/${env.PGdatabaseName}`;
 
 	const connectionString =
-		env.NODE_ENV === 'production' ? env.DATABASE_URL : dev_test;
+	env.NODE_ENV === 'production' ? env.DATABASE_URL : dev_test;
+	const dbConfig = connectionString(connectionString).parser;
 
 	try {
-		const dbPool = new Pool({ connectionString });
+		const dbPool = new Pool(dbConfig);
 
 		/** Create a single database instace for the entire app
 		 * @see https://node-postgres.com/features/pooling
@@ -26,7 +28,8 @@ const app = express();
 		app.locals.dbClient = dbPool;
 
 		await new databaseTables().install(dbPool);
-		console.log('string => ',dev_test)
+		console.log('string => ', dbConfig);
+		console.log('database => ', dbPool);
 	} catch (exception) {
 		console.log(exception);
 	}
