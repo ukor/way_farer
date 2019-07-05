e879c74ee172d0fe044511ed6909995077501293
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-05 02:53:34 +0100

    [Delivers #167058820]

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const chai = require('chai');
 const expect = chai.expect;
+const slug = require('shortid');
 const app = require('../app');
 const dummyData = require('./__dummyData.js');
 const request = require('supertest')(app);
@@ -12,7 +13,12 @@ describe('Routes Test', function () {
 		});
 		it('Expect sign up response body to be an object', function (done) {
 			request.post('/v1/auth/signup')
-				.send(dummyData.signup)
+				.send({
+					first_name: `${slug.generate()}`,
+					last_name: `${slug.generate()}`,
+					email: `user_${slug.generate()}@gmail.test`,
+					password: `${slug.generate()}`
+				})
 				.expect(function (response) {
 					expect(response.body).to.be.an('object');
 					expect(response.body).to.be.have.property('status');
