589780845150bf5f6a1a2ffa524b8e0c927a2101
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-06 17:11:29 +0100

    [Fixes #167058823] Sign in route and logic

---
+++ server/controllers/signin.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const signInValidator = require('../middlewares/validators/signin.js');
+const signup = require('../models/signin.js');
+
+router.post('/', function (request, response, next) {
+	try {
+		// do validation
+		const { body } = request;
+		let user = signInValidator(body);
+
+		request.body = user;
+		next();
+	} catch (exception) {
+		next(exception);
+	}
+}, async function (request, response, next) {
+		try {
+			// do sign in
+			let user = await new signup(request.body, dbClient).authorize();
+			response.json({
+				status: 'success',
+				data: user,
+			});
+		} catch (exception) {
+			next(exception);
+		}
+});
+
+module.exports = router;
