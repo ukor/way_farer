042b53115f3d0b83e45a92d169ed820f73f28938
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-07 20:47:17 +0100

    Lint configuration

--- server/models/signin.js
+++ server/models/signin.js
@@ -1,59 +1,81 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const user = require('./user.js');
 const customError = require('../errorHandles/wayFarerError.js');
-const jwt = require('jsonwebtoken');
 const appConfig = require('../../package.json');
 
-
 class SignIn {
+  constructor(signinDetails, dbClient) {
+    this._userDetails = signinDetails;
+    this._dbClient = dbClient;
+  }
 
-	constructor(signinDetails, dbClient) {
-		this._userDetails = signinDetails;
-		this._dbClient = dbClient;
-	}
+  async _beforeAuthorizing() {
+    // fetch user from db using email address
+    const ud = await new user(this._userDetails.email).fetch(
+      'email',
+      this._dbClient,
+    );
+    if (ud.length <= 0) {throw new customError(
+				'Incorrect email address or password.',
+				'userError',
+				400
+			);}
+    const {
+ password, slug, is_admin, first_name, last_name
+} = ud[0];
+    // compare password hash
+    const isPasswordMatch = await bcrypt.compare(
+      this._userDetails.password,
+      password,
+    );
 
-	async _beforeAuthorizing() {
-		// fetch user from db using email address
-		const ud = await new user(this._userDetails.email).fetch('email', this._dbClient);
-		if (ud.length <= 0) throw new customError('Incorrect email address or password.', 'userError', 400);
-		const { password, slug, is_admin, first_name, last_name } = ud[0];
-		// compare password hash
-		const isPasswordMatch = await bcrypt.compare(this._userDetails.password, password);
+    if (!isPasswordMatch) {throw new customError(
+				'Incorrect email address or password.',
+				'userError',
+				400
+			);}
 
-		if (!isPasswordMatch) throw new customError('Incorrect email address or password.', 'userError', 400);
+    return {
+      slug,
+      is_admin,
+      first_name,
+      last_name,
+    };
+  }
 
-		return {
-			slug, is_admin, first_name, last_name
-		};
-	}
+  _afterAuthorizing(userSlug, token, is_admin) {
+    return {
+      userId: userSlug,
+      token,
+      isAdmin: is_admin,
+    };
+  }
 
-	_afterAuthorizing(userSlug, token, is_admin) {
-		return {
-			userId: userSlug,
-			token,
-			isAdmin: is_admin
-		};
-	}
+  async authorize() {
+    const authenticate = await this._beforeAuthorizing();
 
-	async authorize() {
+    const token = await jwt.sign(
+      {
+        firstName: authenticate.first_name,
+        lastName: authenticate.last_name,
+        userId: authenticate.slug,
+        date: new Date(),
+      },
+      process.env.jwtSecret,
+      {
+        expiresIn: '12h',
+        audience: `${authenticate.first_name} ${authenticate.last_name}`,
+        issuer: appConfig.name,
+      },
+    );
 
-		const authenticate = await this._beforeAuthorizing();
-
-		const token = await jwt.sign({
-			firstName: authenticate.first_name,
-			lastName: authenticate.last_name,
-			userId: authenticate.slug,
-			date: new Date(),
-		}, process.env.jwtSecret, {
-				expiresIn: '12h',
-				audience: `${authenticate.first_name} ${authenticate.last_name}`,
-				issuer: appConfig.name,
-			}
-		);
-
-		return this._afterAuthorizing(authenticate.slug, token, authenticate.is_admin);
-
-	}
+    return this._afterAuthorizing(
+      authenticate.slug,
+      token,
+      authenticate.is_admin,
+    );
+  }
 }
 
 module.exports = SignIn;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,7 @@
+const signup = require('../controllers/signup.js');
+const signin = require('../controllers/signin.js');
+
 module.exports = function (app) {
-	app.use('/v1/auth/signup', require('../controllers/signup.js'));
-	app.use('/v1/auth/signin', require('../controllers/signin.js'));
+  app.use('/v1/auth/signup', signup);
+  app.use('/v1/auth/signin', signin);
 };
