0fe13b41bf0d0911749ae5b4a76c28ef8775636c
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-07 22:38:40 +0100

    Created Trip class

---
+++ server/models/trip.js
@@ -0,0 +1,45 @@
+class Trip {
+  constructor(dbClient) {
+    this.dbClient = dbClient;
+  }
+
+  async save(tripDetails) {
+    this.td = tripDetails;
+    const sql = 'INSERT INTO trips(slug, bus_slug, origin, destination, trip_date, fare, created_by, status, date_created) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)';
+    const values = [
+      this.td.slug, this.td.bus_slug,
+      this.td.origin, this.td.destination,
+      this.td.trip_date, this.td.fare,
+      this.td.created_by, this.td.status,
+      this.td.date_created,
+    ];
+
+    const query = await this.dbClient.query(sql, values);
+
+    return query.rowCount;
+  }
+
+  beforeFetch(filterType) {
+    this.ft = filterType;
+    if (this.ft === 'origin') {
+      return `SELECT * FROM trips WHERE origin = $1`;
+    } if (this.ft === 'destination') {
+      return `SELECT * FROM users WHERE destination = $1`;
+    } else if (this.ft === 'id') {
+      return `SELECT * FROM trips WHERE slug = $1`;
+    } else {
+      return `SELECT * FROM trips WHERE bus_slug = $1`;
+    }
+  }
+
+  fetch(filterType = 'origin' || 'destination' || 'id' || 'bus', filter) {
+    this.filter = filter;
+    this.ft = filterType;
+  }
+
+  update(filter) { }
+
+  remove(filter) { }
+}
+
+module.exports = Trip;
