9eafc6af62ee20775291176d45d79e8a465615bd
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-09 01:42:23 +0100

    Fix linting errors [Fixes #167058833]

--- server/models/trip.js
+++ server/models/trip.js
@@ -1,3 +1,5 @@
+/* eslint-disable quotes */
+/* eslint-disable no-else-return */
 class Trip {
   constructor(dbClient) {
     this.dbClient = dbClient;
@@ -7,10 +9,14 @@ class Trip {
     this.td = tripDetails;
     const sql = 'INSERT INTO trips(slug, bus_slug, origin, destination, trip_date, fare, created_by, status, date_created) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)';
     const values = [
-      this.td.slug, this.td.bus_slug,
-      this.td.origin, this.td.destination,
-      this.td.trip_date, this.td.fare,
-      this.td.created_by, this.td.status,
+      this.td.slug,
+      this.td.bus_slug,
+      this.td.origin,
+      this.td.destination,
+      this.td.trip_date,
+      this.td.fare,
+      this.td.created_by,
+      this.td.status,
       this.td.date_created,
     ];
 
@@ -23,7 +29,8 @@ class Trip {
     this.ft = filterType;
     if (this.ft === 'origin') {
       return `SELECT * FROM trips WHERE origin = $1`;
-    } if (this.ft === 'destination') {
+    }
+    if (this.ft === 'destination') {
       return `SELECT * FROM users WHERE destination = $1`;
     } else if (this.ft === 'id') {
       return `SELECT * FROM trips WHERE slug = $1`;
@@ -32,14 +39,30 @@ class Trip {
     }
   }
 
-  fetch(filterType = 'origin' || 'destination' || 'id' || 'bus', filter) {
+  async fetch(filterType = 'origin' || 'destination' || 'id' || 'bus', filter) {
     this.filter = filter;
-    this.ft = filterType;
+    const sql = this.beforeFetch(filterType);
+    const values = [filter];
+
+    const query = await this.dbClient.query(sql, values);
+
+    return query.rows;
   }
 
-  update(filter) { }
+  update(filter, data) {
+    this.ft = filter;
+    this.ud = data;
+  }
 
-  remove(filter) { }
+  async remove(filter) {
+    this.filter = filter;
+    const sql = `DELETE FROM trips WHERE slug = $1`;
+    const values = [this.filter];
+
+    const query = await this.dbClient.query(sql, values);
+
+    return query.rowCount;
+  }
 }
 
 module.exports = Trip;
