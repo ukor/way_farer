e4fc887058fff729691d19b9d6fb4e54563829fc
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-13 14:21:18 +0100

    Test suite for trip validator

--- test/validators.test.js
+++ test/validators.test.js
@@ -1,50 +1,63 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-undef */
 require('dotenv').config();
 const chai = require('chai');
-const expect = chai.expect;
+
+const { expect } = chai;
 const dummyData = require('./__dummyData.js');
 const signup = require('../server/middlewares/validators/signup.js');
 const signin = require('../server/middlewares/validators/signin.js');
+const addTrip = require('../server/middlewares/validators/trip.js');
 
-describe('Middlewares Validators', function () {
+describe('Middlewares Validators', () => {
+  describe('Signup validator', () => {
+    it('Expect return value to be an object', function () {
+      const v = signup(dummyData.signup);
+      expect(v).to.be.an('object');
+      expect(v).to.have.property('email');
+      expect(v).to.have.property('password');
+    });
 
-	describe('Signup validator', function () {
+    it('Expect error to be thrown', function () {
+      delete dummyData.signup.password;
 
-		it('Expect return value to be an object', function () {
-			const v = signup(dummyData.signup);
-			expect(v).to.be.an('object');
-			expect(v).to.have.property('email');
-			expect(v).to.have.property('password');
-		});
+      expect(function () {
+        signup(dummyData.signup);
+      }).to.throw();
+    });
+  });
 
-		it('Expect error to be thrown', function () {
+  describe('SignIn validator', () => {
+    it('Expect return value to be an object', function () {
+      const v = signin(dummyData.signIn);
+      expect(v).to.be.an('object');
+      expect(v).to.have.property('email');
+      expect(v).to.have.property('password');
+    });
 
-			delete dummyData.signup.password;
+    it('Expect error to be thrown', function () {
+      delete dummyData.signIn.password;
 
-			expect(function () {
-				signup(dummyData.signup);
-			}).to.throw();
-		});
+      expect(function () {
+        signin(dummyData.signIn);
+      }).to.throw();
+    });
+  });
 
-	});
+  describe('Add Trip Validator', () => {
+    it('Expect return value to be an object', function () {
+      const v = addTrip(dummyData.trip);
+      expect(v).to.be.an('object');
+      expect(v).to.have.property('bus_id');
+      expect(v).to.have.property('created_by');
+    });
 
-	describe('SignIn validator', function () {
+    it('Expect error to be thrown', function () {
+      delete dummyData.trip.created_by;
 
-		it('Expect return value to be an object', function () {
-			const v = signin(dummyData.signIn);
-			expect(v).to.be.an('object');
-			expect(v).to.have.property('email');
-			expect(v).to.have.property('password');
-		});
-
-		it('Expect error to be thrown', function () {
-
-			delete dummyData.signIn.password;
-
-			expect(function () {
-				signin(dummyData.signIn);
-			}).to.throw();
-		});
-
-	});
-
+      expect(function () {
+        addTrip(dummyData.trip);
+      }).to.throw();
+    });
+  });
 });
