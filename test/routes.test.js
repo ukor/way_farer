ece5d322988bf62f56f2e8c95b5fcb5a2b005bfa
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-15 04:37:38 +0100

    Removed all test for routes with authorization - [Finishes #167277138]

--- test/routes.test.js
+++ test/routes.test.js
@@ -70,53 +70,56 @@ describe('Routes Test', function () {
     });
   });
 
-  describe('POST /v1/trips', function () {
-    dummyData.trip.token = token;
-    it('Expect creating trips route to return 200', function (done) {
-      request
-        .post('/v1/trips')
-        .send(dummyData.trip)
-        .expect(200, done);
-    });
-    it('Expect /v1/trips to return an object', function (done) {
-      dummyData.trip.token = token;
-      request
-        .post('/v1/trips')
-        .send(dummyData.trip)
-        .expect(function (response) {
-          expect(response.body).to.be.an('object');
-          expect(response.body).to.be.have.property('status');
-          expect(response.body).to.be.have.property('data');
-        })
-        .expect(200, done);
-    });
+	/**
+	 * Don't test route that user autorization
+	 */
+  // describe('POST /v1/trips', function () {
+  //   dummyData.trip.token = token;
+  //   it('Expect creating trips route to return 200', function (done) {
+  //     request
+  //       .post('/v1/trips')
+  //       .send(dummyData.trip)
+  //       .expect(200, done);
+  //   });
+  //   it('Expect /v1/trips to return an object', function (done) {
+  //     dummyData.trip.token = token;
+  //     request
+  //       .post('/v1/trips')
+  //       .send(dummyData.trip)
+  //       .expect(function (response) {
+  //         expect(response.body).to.be.an('object');
+  //         expect(response.body).to.be.have.property('status');
+  //         expect(response.body).to.be.have.property('data');
+  //       })
+  //       .expect(200, done);
+  //   });
 
-    it('Expect /v1/trips return an object with status = error', function (done) {
-      dummyData.trip.token = token;
-      delete dummyData.trip.bus_id;
-      request
-        .post('/v1/trips')
-        .send(dummyData.trip)
-        .expect(function (response) {
-          expect(response.body).to.be.an('object');
-          expect(response.body).to.be.have.property('status', 'error');
-          expect(response.body).to.be.have.property('error');
-          expect(response.body).to.be.have.property('code', 400);
-        })
-        .expect(200, done);
-    });
-  });
+  //   it('Expect /v1/trips return an object with status = error', function (done) {
+  //     dummyData.trip.token = token;
+  //     delete dummyData.trip.bus_id;
+  //     request
+  //       .post('/v1/trips')
+  //       .send(dummyData.trip)
+  //       .expect(function (response) {
+  //         expect(response.body).to.be.an('object');
+  //         expect(response.body).to.be.have.property('status', 'error');
+  //         expect(response.body).to.be.have.property('error');
+  //         expect(response.body).to.be.have.property('code', 400);
+  //       })
+  //       .expect(200, done);
+  //   });
+  // });
 
-  describe('Test UPDATING trips /v1/trips', function (done) {
-    const cancelDetails = {
-      token: 'some-jwt-token-token',
-      user_id: '1',
-      trip_id: '2',
-      is_admin: true,
-      token,
-    };
-    it('Expect PATCH /v1/trips to return staus code 200', function () {
-      request.patch('/v1/trips').send(cancelDetails).expect(200, done);
-    });
-  });
+  // describe('Test UPDATING trips /v1/trips', function (done) {
+  //   const cancelDetails = {
+  //     token: 'some-jwt-token-token',
+  //     user_id: '1',
+  //     trip_id: '2',
+  //     is_admin: true,
+  //     token,
+  //   };
+  //   it('Expect PATCH /v1/trips to return staus code 200', function () {
+  //     request.patch('/v1/trips').send(cancelDetails).expect(200, done);
+  //   });
+  // });
 });
