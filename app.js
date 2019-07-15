26c59a36d16fafd3f8bd033064f3c7d54f97a83e
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-15 11:11:03 +0100

    Log error message in dev and test enviroment - [Delivers #167287261]

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const { Pool } = require('pg');
 const pgConnection = require('pg-connection-string').parse;
-const databaseTables = require('./server/models/installDatabaseTable.js');
+const DatabaseTables = require('./server/models/installDatabaseTable.js');
 const routes = require('./server/routes/index.js');
 
-const { Pool } = require('pg');
 const { env } = process;
 
 const app = express();
 
-(async function() {
+(async function () {
   try {
-    const dev_test = `postgres://${env.PGuser}:${env.PGpassword}@${env.PGhost}:${env.PGport}/${env.PGdatabaseName}`;
+    const devTest = `postgres://${env.PGuser}:${env.PGpassword}@${env.PGhost}:${env.PGport}/${env.PGdatabaseName}`;
 
-    const connectionString =
-      env.NODE_ENV === 'production' ? env.DATABASE_URL : dev_test;
+    const connectionString = env.NODE_ENV === 'production' ? env.DATABASE_URL : devTest;
     const dbConfig = pgConnection(connectionString);
     const dbPool = new Pool(dbConfig);
 
@@ -26,11 +26,11 @@ const app = express();
      */
     app.locals.dbClient = dbPool;
 
-    await new databaseTables().install(dbPool);
+    await new DatabaseTables().install(dbPool);
   } catch (exception) {
     console.log(exception);
   }
-})();
+}());
 
 if (env.NODE_ENV === 'production') {
   app.use(helmet());
@@ -46,7 +46,10 @@ app.get('/', async (request, response) => {
 
 routes(app);
 
-app.use(function(error, req, res, next) {
+app.use((error, req, res, next) => {
+  if (env.NODE_ENV !== 'production') {
+    console.log('Error => ', error);
+  }
   res.json({
     status: 'error',
     error: error.message,
