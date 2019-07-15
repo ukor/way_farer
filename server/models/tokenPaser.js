2c17e7bd6498d00bd517b721e55946739d004b72
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-15 01:36:28 +0100

    Authorization token can now parse and validation - [Fixes #167277138]

---
+++ server/models/tokenPaser.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const CustomError = require('../errorHandles/wayFarerError.js');
+
+/**
+ * Get the authorization token from the request
+ * @return string authorization token
+ * @throws exception
+ * */
+function getToken(request) {
+  const { headers, body } = request;
+
+  if (headers.authorization) {
+    const bearerHeader = headers.authorization;
+    if (typeof bearerHeader === 'undefined') throw new CustomError('Pass a valid authorization token.', 'userError', 403);
+    // split at space
+    const bearer = bearerHeader.split(' ');
+    // return authorization token
+    return bearer[1];
+  }
+  return body.token;
+}
+
+/**
+ * Verify authorization token
+ * @return boolean
+ * @throws exception
+ *  */
+async function verifyToken(request) {
+  const token = getToken(request);
+  const verificationToken = await jwt.verify(token, process.env.jwtSecret);
+  if (!verificationToken) throw new CustomError('403 - Forbidden, Unauthorized', 'userError', 403);
+
+  return true;
+}
+
+module.exports = verifyToken;
